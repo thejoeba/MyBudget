ace889e2af2656b1756c39ab16a2b47692941a48
Joey Heck <joeyh@esub.comgit config --global user.email joeyh@esub.com>
2018-01-15 12:24:53 -0800

    Converted TransactionActivity to Java

---
+++ app/src/main/java/com/cashmeoutside/mybudget/activities/TransactionActivity.java
@@ -0,0 +1,15 @@
+package com.cashmeoutside.mybudget.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.cashmeoutside.mybudget.R;
+
+public class TransactionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transaction);
+    }
+}
